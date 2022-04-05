875e1a04ea61737421e3ae4e8c29dd51ab15b176
Brandon Helt <brandon.helt@yahoo.com>
2022-04-05 11:38:30 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter((element) => name.toLowerCase() === element.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter((element) => element.substring(0, query.length) === query);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((element) => element.name === name);
+}
